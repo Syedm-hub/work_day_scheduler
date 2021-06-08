1acd8bf3df899d51b7fe91aa69961c34a1c7809e
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-06-07 21:53:06 -0500

    updated with Script code

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ const lastEntry =17;
 
 const hourMap = ["12AM","1AM","2AM","3AM","4AM","5AM","6AM","7AM","8AM","9AM","10AM","11AM","12PM",
 "1PM","2PM","3PM","4PM","5PM","6PM","7PM","8PM","9PM","10PM","11PM"];
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", 
+"July", "August", "September", "October", "November", "December"];
+
+setCurrentDateAndHour();
+buildTimeBlocks();
+getTimeEntries();
+
+$(".saveBtn").click(saveClick);
+
+function setCurrentDateAndHour() {
+    var today = new Date();
+    var day = today.getDate();
+    var dayEnd = "th";
+}
+
+currentHour = today.getHours();
