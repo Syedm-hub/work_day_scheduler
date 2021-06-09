b431d77c9837e172ad8c25a14a31dcc0bdd2f04e
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-06-08 19:22:17 -0500

    updated

--- script.js
+++ script.js
@@ -1,5 +1,77 @@
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
+var currentDay = moment().format("dddd LL");
+$("#currentDay").append(currentDay);
+
+var currentTime = moment().format("LT");
+$("#currentTime").append(currentTime);
+
+
+var now = new Date().getHours();
+
+function colorCode() {
+  if (now > 9) {
+    $("#comment9am").addClass("past");
+  } else if (now >= 9 && now < 10) {
+    $("#comment9am").addClass("present");
+  } else if (now < 9) {
+    $("#comment9am").addClass("future");
+  }
+  if (now > 10) {
+    $("#comment10am").addClass("past");
+  } else if (now >= 10 && now < 11) {
+    $("#comment10am").addClass("present");
+  } else if (now < 10) {
+    $("#comment10am").addClass("future");
+  }
+  if (now > 11) {
+    $("#comment11am").addClass("past");
+  } else if (now >= 11 && now < 12) {
+    $("#comment11am").addClass("present");
+  } else if (now < 11) {
+    $("#comment11am").addClass("future");
+  }
+  if (now > 12) {
+    $("#comment12pm").addClass("past");
+  } else if (now >= 12 && now < 13) {
+    $("#comment12pm").addClass("present");
+  } else if (now < 12) {
+    $("#comment12pm").addClass("future");
+  }
+  if (now > 13) {
+    $("#comment1pm").addClass("past");
+  } else if (now >= 13 && now < 14) {
+    $("#comment1pm").addClass("present");
+  } else if (now < 13) {
+    $("#comment1pm").addClass("future");
+  }
+  if (now > 14) {
+    $("#comment2pm").addClass("past");
+  } else if (now >= 14 && now < 15) {
+    $("#comment2pm").addClass("present");
+  } else if (now < 14) {
+    $("#comment2pm").addClass("future");
+  }
+  if (now > 15) {
+    $("#comment3pm").addClass("past");
+  } else if (now >= 15 && now < 16) {
+    $("#comment3pm").addClass("present");
+  } else if (now < 15) {
+    $("#comment3pm").addClass("future");
+  }
+  if (now > 16) {
+    $("#comment4pm").addClass("past");
+  } else if (now >= 16 && now < 17) {
+    $("#comment4pm").addClass("present");
+  } else if (now < 16) {
+    $("#comment4pm").addClass("future");
+  }
+  if (now > 17) {
+    $("#comment5pm").addClass("past");
+  } else if (now >= 17 && now < 18) {
+    $("#comment5pm").addClass("present");
+  } else if (now < 17) {
+    $("#comment5pm").addClass("future");
+  }
+}
 
 
 var currentDate = ""; 
@@ -15,8 +87,7 @@ const hourMap = ["12AM","1AM","2AM","3AM","4AM","5AM","6AM","7AM","8AM","9AM","1
 "1PM","2PM","3PM","4PM","5PM","6PM","7PM","8PM","9PM","10PM","11PM"];
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["January", "February", "March", "April", "May", "June", 
-"July", "August", "September", "October", "November", "December"];
+
 
 setCurrentDateAndHour();
 buildTimeBlocks();
